f96cbab362dcdefbea79c1332de9ec73819099f4
Chandan <chandancvp08@gmail.com>
2023-04-13 00:02:13 +0530

    mandatory commit

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"notice_board", origin, size)) {
+  if (!window.CreateAndShow(L"MAHE BLR Notice Board", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
