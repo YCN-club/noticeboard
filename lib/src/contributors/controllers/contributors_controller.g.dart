2be6a6c692af7b6cbac5f360975c8bd1e994aa91
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-03-08 07:49:04 +0530

    feat: move `contributors_controller` to new model

---
+++ lib/src/contributors/controllers/contributors_controller.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'contributors_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$contributorsHash() => r'f9c11d2ea2ff87a47d3e73e79db5cb5f2576d7b4';
+
+/// See also [Contributors].
+@ProviderFor(Contributors)
+final contributorsProvider = AutoDisposeAsyncNotifierProvider<Contributors,
+    List<ContributorModel>>.internal(
+  Contributors.new,
+  name: r'contributorsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$contributorsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Contributors = AutoDisposeAsyncNotifier<List<ContributorModel>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
---
+++ lib/src/contributors/views/contributors_view.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'package:noticeboard/src/contributors/controllers/contributors_controller.dart';
+
+class ContributorsView extends ConsumerWidget {
+  const ContributorsView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final contributors = ref.watch(contributorsProvider);
+
+    return contributors.when(
+      data: (data) => ListView.builder(
+        itemCount: data.length,
+        itemBuilder: ((context, index) {
+          return ListTile(
+            trailing: IconButton(
+              onPressed: () {
+                launchUrl(Uri.parse(data[index].htmlUrl));
+              },
+              icon: const Icon(Icons.open_in_new),
+            ),
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(data[index].avatarUrl),
+            ),
+            title: Text(
+              data[index].login,
+              style: const TextStyle(
+                  color: Colors.black, fontWeight: FontWeight.w500),
+            ),
+            subtitle: Text(
+                '${data[index].contributions.toString()} contributions',
+                style: const TextStyle(color: Colors.grey)),
+          );
+        }),
+      ),
+      loading: () => const Center(child: CircularProgressIndicator()),
+      error: (error, stackTrace) => Center(
+        child: Text('Contributors could not be fetched.'),
+      ),
+    );
+  }
+}
