db5db4fcea2b93386d0366ec82ed6ed5a8adcc73
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-03-08 07:58:21 +0530

    chore(ui): init components under new model

---
+++ lib/src/notices/views/events_view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class EventsView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
